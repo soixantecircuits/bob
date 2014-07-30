9b394baca334e210b27a95dfc990a4e2aa4e446e
Arthur Battut <arthur@soixantecircuits.fr>
2014-07-30 11:16:55 +0200

    retrieve one vent

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var http = require('http'),
     auth = require('./credentials.js').auth,
     request = require('request'),
-    Twitter = require('node-twitter');
+    Twitter = require('node-twitter'),
+    wiki = require('nodemw');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -9,25 +10,88 @@ http.createServer(function (req, res) {
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
-var options = {
-  host: 'en.wikipedia.org',
-  path: '/w/api.php?action=query&prop=info&inprop=url&format=json&titles=France'
-};
-
-http.get(options, function(resp){
-	resp.on('data', function(chunk){
-		var data = JSON.parse(chunk).query.pages;
-		for (var key in data) {
-		  if (data.hasOwnProperty(key)) {
-		  	var titre = data[key].title,
-		  		url = data[key].fullurl;
-                console.log(titre, url);
-		  }
-		}
-	});
-}).on("error", function(e){
-	console.log("Got error: " + e.message);
+// pass configuration object
+var clientWiki = new wiki({
+  server: 'en.wikipedia.org',  // host name of MediaWiki-powered site
+  path: '/w',                  // path to api.php script
+  debug: false                // is more verbose when set to true
 });
+
+// Get one event from a date
+function getPageYearWiki(date){
+	clientWiki.getArticle(date, function(data) {
+		// Découpe l'article en plusieurs morceaux
+		var array = data.split("== Events =="),
+			eventsBrut = [],
+			eventsClean = [],
+			isGood = false,
+			eventFinal;
+
+
+		array = array[1];
+		array = array.split("==");
+
+		// Obtenir tous les events, mais dans des catégories
+		for(i=0; i<array.length; i++){
+			if(array[i].length>100) {
+				var item = array[i].split("*");
+				eventsBrut.push(item);
+			}
+		}
+		// Obtenir juste la liste de tous les events
+		for(i=0; i<eventsBrut.length; i++){
+			for(key in eventsBrut[i]){
+				var item = eventsBrut[i][key];
+				if(item.length>0){
+					eventsClean.push(item);
+					// On rempli avec les items
+				}
+			}
+		}
+		// Obtenir un event qui soit plus long que 30 charactères
+		do {
+			var indexRandom =  Math.floor(Math.random() * (eventsClean.length - 0 + 1) + 0);
+			var item = eventsClean[indexRandom];
+			if(item.length>30){
+				eventFinal = item;
+				isGood = true;
+			}
+		} while (!isGood);
+
+		// on enlève tous les [ et les ]
+		eventFinal = eventFinal.replace(/[\[\]]+/g, '');
+		// on enlèves tous les chaines qui commencent par & et qui finissent par ;
+		eventFinal = eventFinal.replace(/&[a-z]+;/g, '');
+
+		console.log(eventFinal);
+	});
+}
+
+getPageYearWiki("1515");
+
+// Get the page name and his url
+function searchOnePageWiki(pageName){
+	var options = {
+	  host: 'en.wikipedia.org',
+	  path: '/w/api.php?action=query&prop=info&inprop=url&format=json&titles='+pageName,
+	};
+	http.get(options, function(resp){
+		resp.on('data', function(chunk){
+			var data = JSON.parse(chunk).query.pages;
+			for (var key in data) {
+			  if (data.hasOwnProperty(key)) {
+			  	var titre = data[key].title,
+			  		url = data[key].fullurl;
+			  	console.log(titre, url);
+			  }
+			}
+		});
+	}).on("error", function(e){
+		console.log("Got error: " + e.message);
+	});
+
+}
+
 
 var nytBaseUrl = "http://api.nytimes.com",
     nytFormat = ".json",
@@ -38,14 +102,14 @@ var nytBaseUrl = "http://api.nytimes.com",
     nytFieldList = "&fl=headline,web_url,multimedia",
     nytAPIKey = "&api-key=" + auth.NYT.KEY;
 
-var nytReqOpt = nytBaseUrl + nytBasePath + nytBeginDate + nytEndDate + nytSort + nytFieldList + nytAPIKey;
+// var nytReqOpt = nytBaseUrl + nytBasePath + nytBeginDate + nytEndDate + nytSort + nytFieldList + nytAPIKey;
 
-request(nytReqOpt, function (error, response, body) {
-    if(error) console.log(error);
-    articles = JSON.parse(body).response.docs;
-    // console.log(articles);
-    getRandomArticle(articles);
-});
+// request(nytReqOpt, function (error, response, body) {
+//     if(error) console.log(error);
+//     articles = JSON.parse(body).response.docs;
+//     // console.log(articles);
+//     getRandomArticle(articles);
+// });
 
 function getRandomArticle(dataset){
     var lucky = Math.floor(Math.random() * dataset.length);
